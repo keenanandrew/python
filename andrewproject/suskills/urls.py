c40a761fa4a3835efbed7c698a19e1f236d62852
keenanandrew <63597895+keenanandrew@users.noreply.github.com>
2020-06-04 14:07:19 +0100

    Creating New Year app

--- andrewproject/suskills/urls.py
+++ andrewproject/suskills/urls.py
@@ -3,5 +3,6 @@ from . import views # import views from current directory
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("andrew", views.andrew, name="andrew") # the index function I created in views.py
+    path("suskills", views.suskills, name="suskills"), # the index function I created in views.py
+    path("<str:name>", views.greet, name="greet")
 ]
--- andrewproject/suskills/views.py
+++ andrewproject/suskills/views.py
@@ -5,7 +5,14 @@ from django.shortcuts import render
 
 # this function represents a view; it returns "Hello, folks" as an http response
 def index(request):
-    return HttpResponse("Hello there, folks!")
+    return render(request, "suskills/index.html")
+    # renders an html page, which can be edited with variables etc
 
-def andrew(request):
+def suskills(request):
     return HttpResponse("Hello, Andrew")
+
+def greet(request, name):
+    # the {} contents are the 'context' - all of the info/variables being provided to the template
+    return render(request, "suskills/greet.html", {
+        "name": name.capitalize()    
+    })
