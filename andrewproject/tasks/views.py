d4c775c2a21fa05be3f35508f16486fb94aff068
keenanandrew <63597895+keenanandrew@users.noreply.github.com>
2020-06-09 10:49:52 +0100

    Binary changes

--- andrewproject/tasks/views.py
+++ andrewproject/tasks/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django import forms
 
 # sample tasks
 tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 # Create your views here.
 def index(request):
@@ -10,4 +15,15 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
